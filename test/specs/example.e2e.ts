ce766604b39efce8bcc3d404d8ebf61da1434ad3
Eugene Pisny <eugene.pisny@namecheap.com>
2023-03-10 19:44:08 +0200

    fix

--- test/specs/example.e2e.ts
+++ test/specs/example.e2e.ts
@@ -5,7 +5,7 @@ describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         await LoginPage.open();
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        await LoginPage.login('tomsmith', '');
         await expect(SecurePage.flashAlert).toBeExisting();
         await expect(SecurePage.flashAlert).toHaveTextContaining('You logged into a secure area!');
     });
