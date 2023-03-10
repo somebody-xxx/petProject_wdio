a0ef57353f4846a9ae1fce29091fa7e2c8f9ab5c
Eugene Pisny <eugene.pisny@namecheap.com>
2023-03-10 19:44:50 +0200

    fix: add user password

--- test/specs/example.e2e.ts
+++ test/specs/example.e2e.ts
@@ -5,7 +5,7 @@ describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         await LoginPage.open();
 
-        await LoginPage.login('tomsmith', '');
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
         await expect(SecurePage.flashAlert).toBeExisting();
         await expect(SecurePage.flashAlert).toHaveTextContaining('You logged into a secure area!');
     });
