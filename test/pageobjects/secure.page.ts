832bcafe3ed7286387af7c8346fc93e1a672d3e9
Eugene Pisny <eugene.pisny@namecheap.com>
2023-03-11 19:13:50 +0200

    feat: delete js dock

--- test/pageobjects/secure.page.ts
+++ test/pageobjects/secure.page.ts
@@ -6,9 +6,6 @@ import Page from './page';
  * sub page containing specific selectors and methods for a specific page
  */
 class SecurePage extends Page {
-    /**
-     * define selectors using getter methods
-     */
     public get flashAlert(): ChainablePromiseElement<Promise<WebdriverIO.Element>> {
         return $('#flash');
     }
