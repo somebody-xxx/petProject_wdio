5a0942ddfe2fa72c23ea6a72b77368cef2f6f9a2
Eugene Pisny <eugene.pisny@namecheap.com>
2023-03-11 17:52:29 +0200

    add space to js dock

--- test/pageobjects/page.ts
+++ test/pageobjects/page.ts
@@ -4,8 +4,10 @@
 */
 export default class Page {
     /**
+     *
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
+     *
     */
     public open(path: string): Promise<string> {
         return browser.url(`https://the-internet.herokuapp.com/${path}`)
