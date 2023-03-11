7bfe77c5c944fd6be2ee1473037ccccc144a35eb
Eugene Pisny <eugene.pisny@namecheap.com>
2023-03-11 20:05:56 +0200

    feat: add js dock

--- test/pageobjects/secure.page.ts
+++ test/pageobjects/secure.page.ts
@@ -6,6 +6,9 @@ import Page from './page';
  * sub page containing specific selectors and methods for a specific page
  */
 class SecurePage extends Page {
+    /**
+    * define selectors using getter methods
+    */
     public get flashAlert(): ChainablePromiseElement<Promise<WebdriverIO.Element>> {
         return $('#flash');
     }
