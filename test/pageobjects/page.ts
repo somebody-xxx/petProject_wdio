3b63b0ad470535a637216bb00f849039b6c7703a
Eugene Pisny <eugene.pisny@namecheap.com>
2023-03-11 19:11:51 +0200

    feat: remove e.g.'

--- test/pageobjects/page.ts
+++ test/pageobjects/page.ts
@@ -5,7 +5,7 @@
 export default class Page {
     /**
     * Opens a sub page of the page
-    * @param path path of the sub page (e.g. /path/to/page.html)
+    * @param path path of the sub page
     */
     public open(path: string): Promise<string> {
         return browser.url(`https://the-internet.herokuapp.com/${path}`)
